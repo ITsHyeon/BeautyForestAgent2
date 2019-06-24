0c7e1af25618f75e99662a5b9dd02054844f70d8
Suhyeon Lee <suhyeon917917@gmail.com>
2019-06-24 09:53:19 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3512 이수현

--- BeautyForestAgent/Form1.cs
+++ BeautyForestAgent/Form1.cs
@@ -20,11 +20,7 @@ namespace BeautyForestAgent
             this.Visible = visible;
             this.ntiTray.Visible = !visible;
         }
-        public string Title
-        {
-            set { this.Text = value; }
-        }
-
+        
         private void 폼보이기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             VisibleChange(true);
@@ -54,19 +50,52 @@ namespace BeautyForestAgent
 
         private void 끝내기ToolStripMenuItem1_Click(object sender, EventArgs e)
         {
+            this.statusMessage.Text = "종료합니다.";
             Application.ExitThread();  // 어플리케이션 쓰레드 완전 종료
         }
 
         private void 수정ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ShowModal();
+            ShowModalEdit();
         }
 
-        private static void ShowModal()
+        private void ShowModalEdit()
         {
+            this.statusMessage.Text = "프로필 수정";
             FormModify m = new FormModify();
-            m.Title = "모달 창띄우기";
+            m.Title = "프로필 수정 창띄우기";
             m.ShowDialog();
+        }
+
+        private void 추가ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModalAdd();
+        }
+
+        private void ShowModalAdd()
+        {
+            this.statusMessage.Text = "프로필 추가";
+            FormAdd m = new FormAdd();
+            m.Title = "프로필 추가 창 띄우기";
+            m.ShowDialog();
+        }
+
+        private void 이프로그램은ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "Modaless창을 띄웁니다.";
+            FormAbout m = new FormAbout();
+            m.Title = "Modaless 창 띄우기";
+            m.Show();
+        }
+
+        private void BtnModify_Click(object sender, EventArgs e)
+        {
+            ShowModalEdit();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            ShowModalAdd();
         }
     }
 
